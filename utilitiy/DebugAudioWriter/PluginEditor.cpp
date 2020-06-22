98e08bb874ce17347bf34f7039ab92cfb8a2ab09
Joerg Bitzer <joerg.bitzer@jade-hs.de>
2020-06-22 22:33:21 +0200

    add AudioWriter Utilty

---
+++ utilitiy/DebugAudioWriter/PluginEditor.cpp
@@ -0,0 +1,119 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+DebugAudioWriterAudioProcessorEditor::DebugAudioWriterAudioProcessorEditor (DebugAudioWriterAudioProcessor& p)
+    : AudioProcessorEditor (&p), processor (p), m_insertclick(false)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+
+	m_button.setButtonText("Save");
+	m_button.onClick = [this]() {saveAudio(); };
+	addAndMakeVisible(m_button);
+
+	m_insertButton.setButtonText("set Endmark");
+	m_insertButton.onClick = [this]() {insertClick(); };
+	addAndMakeVisible(m_insertButton);
+
+    setSize (300, 100);
+}
+
+DebugAudioWriterAudioProcessorEditor::~DebugAudioWriterAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void DebugAudioWriterAudioProcessorEditor::paint (Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+	if (!m_insertclick)
+	{
+		m_insertButton.setColour(TextButton::ColourIds::buttonColourId, Colours::darkgrey);
+		m_insertButton.setButtonText("set Endmark");
+	}
+	else
+	{
+		m_insertButton.setColour(TextButton::ColourIds::buttonColourId, Colours::darkred);
+		m_insertButton.setButtonText("unset");
+	}
+
+}
+
+void DebugAudioWriterAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+
+	m_button.setBounds(10, 20, 180, 60);
+	m_insertButton.setBounds(200, 20, 90, 60);
+}
+
+void DebugAudioWriterAudioProcessorEditor::saveAudio()
+{
+//	ScopedLock(processor.m_writer);
+
+	Time timeobj;
+
+	auto tt = timeobj.getCurrentTime();
+
+	auto ms = tt.toMilliseconds();
+	String  dirname= "c:\\temp\\";
+	auto outfilename = String(dirname + String(ms) + ".wav");
+	File outfile(outfilename);
+	auto fileStream = std::unique_ptr<FileOutputStream>(outfile.createOutputStream());
+	WavAudioFormat wavFormat;
+	
+	AudioFormatWriter *writer;
+	if (processor.m_nrofchans == 1)
+		writer = wavFormat.createWriterFor(fileStream.get(), processor.m_fs, 1, 16, {}, 0);
+	
+	if (processor.m_nrofchans == 2)
+		writer = wavFormat.createWriterFor(fileStream.get(), processor.m_fs, 2, 16, {}, 0);
+
+	fileStream.release();
+
+	std::vector<float> dataL;
+	std::vector<float> dataR;
+
+	processor.m_writer.enter();
+	while(processor.m_dataLeft.size() > 0)
+	{
+		float* pData[2];
+		dataL = processor.m_dataLeft.front();
+		if (m_insertclick)
+			dataL.at(dataL.size()-1) = 1.0;
+		
+		processor.m_dataLeft.pop();
+		pData[0] = &dataL[0];
+		if (processor.m_nrofchans == 2)
+		{
+			dataR = processor.m_dataRight.front();
+			pData[1] = &dataR[0];
+			processor.m_dataRight.pop();
+		}
+		writer->writeFromFloatArrays(pData, processor.m_nrofchans, dataL.size());
+	}
+
+	processor.m_counter = 0;
+	writer->flush();
+	delete writer;
+	processor.m_writer.exit();
+}
+
+void DebugAudioWriterAudioProcessorEditor::insertClick()
+{
+	m_insertclick = !m_insertclick;
+	repaint();
+}
