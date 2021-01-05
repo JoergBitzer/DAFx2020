64e2f02047ad843760aaf4dcc0aa6986cc82d1d2
Joerg Bitzer <joerg.bitzer@jade-hs.de>
2021-01-05 21:24:07 +0100

    Anpassung an CMake und plattform unabhängig

--- utilitiy/DebugAudioWriter/PluginEditor.cpp
+++ utilitiy/DebugAudioWriter/PluginEditor.cpp
@@ -69,7 +69,34 @@ void DebugAudioWriterAudioProcessorEditor::saveAudio()
 	auto tt = timeobj.getCurrentTime();
 
 	auto ms = tt.toMilliseconds();
-	String  dirname= "c:\\temp\\";
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+   //define something for Windows (32-bit and 64-bit, this part is common)
+   String  dirname= "c:\\temp\\";
+#elif __APPLE__
+    #include <TargetConditionals.h>
+    #if TARGET_IPHONE_SIMULATOR
+         // iOS Simulator
+    #elif TARGET_OS_IPHONE
+        // iOS device
+    #elif TARGET_OS_MAC
+        // Other kinds of Mac OS
+		String  dirname= "~/Temp/";
+    #else
+    #   error "Unknown Apple platform"
+    #endif
+#elif __linux__
+	String  dirname= "~/Temp/";
+    // linux
+#elif __unix__ // all unices not caught above
+	String  dirname= "~/Temp/";
+    // Unix
+#elif defined(_POSIX_VERSION)
+    // POSIX
+#else
+#   error "Unknown compiler"
+#endif
+
+	
 	File myFolder(dirname);
 	if (myFolder.isDirectory() == false)
 	{
